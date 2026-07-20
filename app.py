5b6d0e9e43a8d3e10b04e8d5b1fe0c38ab49eec3
Surier Gabriel <gabriel.surier@gmail.com>
2026-07-20 09:02:30 +0200

    Add database with exercises and memory state

--- app.py
+++ app.py
@@ -7,79 +7,60 @@ import duckdb
 import pandas as pd
 import streamlit as st
 from pandas import DataFrame
+from init_db import beverages, food_items, solution_df,ANSWER_STR,con
 
-CSV: str = """
-beverage,price
-orange juice,2.5
-Expresso,2
-Tea,3
-"""
-CSV2: str = """
-food_item,food_price
-cookie juice,2.5
-chocolatine,2
-muffin,3
-"""
-# Test CI : mise en place des dataframes depuis les fichiers csv
-beverages: DataFrame = cast(DataFrame, pd.read_csv(io.StringIO(CSV)))
-food_items: DataFrame = cast(DataFrame, pd.read_csv(io.StringIO(CSV2)))
-
-ANSWER_STR: str = """
-SELECT * FROM beverages
-CROSS JOIN food_items
-"""
-solution_df = duckdb.sql(ANSWER_STR).df()
 
 st.write("""
 # SQL SRS
 Spaced Repetition System SQL practice
 """)
 
-st.header("entrez your code:")
-SQL_QUERY: str = str(st.text_area(label="votre code SQL ici", key="user_input"))
-
 
 with st.sidebar:
     option = st.selectbox(
         "What dog you like to review",
-        ["Joins", "GroupBy", "Windows Functions"],
+        ["Joins", "GroupBy", "Windows Functions","cross_joins"],
         index=None,
         placeholder="Select a theme...",
     )
     st.write("You selected:", option)
+    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme='{option}'")
+    st.write(exercise)
 
+st.header("entrez your code:")
+SQL_QUERY: str = str(st.text_area(label="votre code SQL ici", key="user_input"))
 
-if SQL_QUERY:
-
-    df_duckdb: DataFrame = duckdb.query(SQL_QUERY).df()
-    if len(solution_df.columns) != len(df_duckdb.columns):
-        st.write("Error, some columns are missing")
-    n_lines_difference = df_duckdb.shape[0] - solution_df.shape[0]
-    if n_lines_difference != 0:
-        st.write(f"""result has a {n_lines_difference} lines difference between
-            your solution_df and your query""")
-
-    try:
-        result = df_duckdb[solution_df.columns]
-        if str(result.compare(solution_df)) != "":
-            st.write("Error, the datas are not the same")
-            st.dataframe(result.compare(solution_df))
-    except KeyError as e:
-        st.write("Error, your query is not what we expected")
-
-    st.dataframe(df_duckdb, width=400)
-else:
-    st.write("Vous n'avez pas entrez de query sql.")
-
-
-tab2, tab3 = st.tabs(["Tables", "Solution"])
-
-with tab2:
-    st.write("table: beverages")
-    st.dataframe(beverages)
-    st.write("table: food_items")
-    st.dataframe(food_items)
-    st.write("expected:")
-    st.dataframe(solution_df)
-with tab3:
-    st.write(ANSWER_STR)
+# if SQL_QUERY:
+#
+#     df_duckdb: DataFrame = duckdb.query(SQL_QUERY).df()
+#     if len(solution_df.columns) != len(df_duckdb.columns):
+#         st.write("Error, some columns are missing")
+#     n_lines_difference = df_duckdb.shape[0] - solution_df.shape[0]
+#     if n_lines_difference != 0:
+#         st.write(f"""result has a {n_lines_difference} lines difference between
+#             your solution_df and your query""")
+#
+#     try:
+#         result = df_duckdb[solution_df.columns]
+#         if str(result.compare(solution_df)) != "":
+#             st.write("Error, the datas are not the same")
+#             st.dataframe(result.compare(solution_df))
+#     except KeyError as e:
+#         st.write("Error, your query is not what we expected")
+#
+#     st.dataframe(df_duckdb, width=400)
+# else:
+#     st.write("Vous n'avez pas entrez de query sql.")
+#
+#
+# tab2, tab3 = st.tabs(["Tables", "Solution"])
+#
+# with tab2:
+#     st.write("table: beverages")
+#     st.dataframe(beverages)
+#     st.write("table: food_items")
+#     st.dataframe(food_items)
+#     st.write("expected:")
+#     st.dataframe(solution_df)
+# with tab3:
+#     st.write(ANSWER_STR)
---
+++ init_db.py
@@ -0,0 +1,65 @@
+# pylint: disable=missing-module-docstring
+import io
+
+import duckdb
+import pandas as pd
+from pandas import DataFrame
+from typing import cast
+
+con = duckdb.connect(database="data/exercices_sql_tables.duckdb",read_only=False)
+
+# ------------------------------------------------------------
+# EXERCISES LIST
+# ------------------------------------------------------------
+
+data = {
+    "theme": ["cross_joins","window_functions"],
+    "exercise_name": ["beverages_and_food","simple_window"],
+    "tables": [["beverages", "food_items"],"simple_window"],
+    "last_reviewed": ["1970-01-01","1970-01-01" ]
+}
+memory_state_df = pd.DataFrame(data)
+con.execute("""
+            DROP TABLE IF EXISTS memory_state;
+            CREATE TABLE memory_state AS
+                SELECT * FROM memory_state_df;
+            """)
+
+# ------------------------------------------------------------
+# CROSS JOIN EXERCISES
+# ------------------------------------------------------------
+
+
+CSV: str = """
+beverage,price
+orange juice,2.5
+Expresso,2
+Tea,3
+"""
+CSV2: str = """
+food_item,food_price
+cookie juice,2.5
+chocolatine,2
+muffin,3
+"""
+
+beverages: DataFrame = cast(DataFrame, pd.read_csv(io.StringIO(CSV)))
+
+food_items: DataFrame = cast(DataFrame, pd.read_csv(io.StringIO(CSV2)))
+
+con.execute(f"""
+            DROP TABLE IF EXISTS beverages;
+            CREATE TABLE beverages AS 
+            SELECT * FROM beverages;
+            DROP TABLE IF EXISTS food_items;
+            CREATE TABLE food_items AS 
+            SELECT * FROM food_items;                
+            """)
+
+
+ANSWER_STR: str = """
+SELECT * FROM beverages
+CROSS JOIN food_items
+"""
+solution_df = duckdb.sql(ANSWER_STR).df()
+
